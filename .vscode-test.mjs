9c7d44b9cd86bd3011f3b8fd3de55c818d7f02c7
Pavel Purma <pavel@purma.cz>
2026-03-28 12:48:20 +0100

    build output changes

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "@vscode/test-cli";
 
 export default defineConfig({
-  files: "out/test/suite/**/*.test.js",
+  files: "build/test/suite/**/*.test.js",
   mocha: {
     ui: "tdd",
     timeout: 20000,
